bcae944a40610dcad06e09ff4d06bf3a0c9571dc
sajad1382azari <sajadazari1382@gmail.com>
2024-07-19 01:15:19 +0330

    Update 2.py
    class operand node

--- 2.py
+++ 2.py
@@ -1,1 +1,26 @@
-print("hi") 
+import math
+
+# Base class for tree nodes
+class Node:
+    def evaluate(self, variables):
+        raise NotImplementedError("Subclasses should implement this method!")
+
+# Class for operand nodes (binary operations)
+class OperandNode(Node):
+    def __init__(self, operator, left, right):
+        self.operator = operator
+        self.left = left
+        self.right = right
+
+    def evaluate(self, variables):
+        if self.operator == '+':
+            return self.left.evaluate(variables) + self.right.evaluate(variables)
+        elif self.operator == '-':
+            return self.left.evaluate(variables) - self.right.evaluate(variables)
+        elif self.operator == '*':
+            return self.left.evaluate(variables) * self.right.evaluate(variables)
+        elif self.operator == '/':
+            return self.left.evaluate(variables) / self.right.evaluate(variables)
+        else:
+            raise ValueError(f"Unknown operator: {self.operator}")
+
